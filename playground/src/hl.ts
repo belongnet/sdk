1561128937ef510648616c59c065ac866ab68ddf
reslear <reslear@gmail.com>
2025-03-10 22:20:09 +0100

    refactor: docs

--- playground/src/hl.ts
+++ playground/src/hl.ts
@@ -1,23 +1,11 @@
-import { type HighlighterCore, getHighlighterCore } from 'shiki/core'
-import { shallowRef } from 'vue'
+import { createHighlighterCore } from 'shiki/core'
+import { createJavaScriptRegexEngine } from 'shiki/engine/javascript'
 
-const highlighter = shallowRef<HighlighterCore>()
-
-export function usehighlighter() {
-  if (typeof window !== 'undefined') {
-    getHighlighterCore({
-      themes: [
-        import('shiki/themes/vitesse-dark.mjs'),
-        import('shiki/themes/vitesse-light.mjs'),
-      ],
-      langs: [import('shiki/langs/javascript.mjs')],
-      loadWasm: import('shiki/wasm'),
-    }).then((h) => {
-      highlighter.value = h
-    })
-  }
-
-  return {
-    highlighter,
-  }
-}
+export const highlighter = await createHighlighterCore({
+  themes: [
+    import('shiki/themes/vitesse-dark.mjs'),
+    import('shiki/themes/vitesse-light.mjs'),
+  ],
+  langs: [import('shiki/langs/javascript.mjs')],
+  engine: createJavaScriptRegexEngine(),
+})
